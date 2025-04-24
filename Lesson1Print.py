030a4f92dc628b6b703ec4230e1fc4dc6af50242
Marcus Fernandes <fernandesmarcus.e@gmail.com>
2025-04-24 13:16:32 -0500

    Initial commit with Python Workshop files

---
+++ Lesson1Print.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 24 11:09:22 2025
+
+@author: ferna
+"""
+
+print ("hello world!")
+
+print ("\"hello world!\"")
+#print ("whisper Can you hear me")
+print ("hello" , 3)
+
+print ("hello i am still talking and i do not want to stop i just \
+ant to keep going")
